a46eea9000278dff66cfec74a59db53caf2ac950
Carlos Henrique Lins Ponchirolli <107359480+CarlosHLP060304@users.noreply.github.com>
2023-02-28 00:17:41 -0300

    Arrumei a última linha do arquivo "Criador_Arquivo_Automatizador.bat"

--- Criador_Arquivo_Automatizador.bat
+++ Criador_Arquivo_Automatizador.bat
@@ -3,4 +3,5 @@ mkdir CAMINHO_SQL_TERMINAL
 cd  C:\Users\%USERNAME%\CAMINHO_SQL_TERMINAL
 echo cd C:\Program Files\MySQL\MySQL Server 8.0\bin>Comandos_Para_Usar_SQL_Terminal.bat
 echo mysql -h localhost -u root -p >> Comandos_Para_Usar_SQL_Terminal.bat
-set PATH= %PATH%;C:\Users\%USERNAME%\CAMINHO_SQL_TERMINAL
+setx PATH "%PATH%;C:\Users\%USERNAME%\CAMINHO_SQL_TERMINAL" /M
+
